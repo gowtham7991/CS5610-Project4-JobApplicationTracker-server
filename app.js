126864f38c712bd6599506619c9359104a5ed55d
renuk21 <renukaganesh21@gmail.com>
2022-12-05 19:20:06 -0500

    adzuna 3rd party api changes

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from "mongoose"
+import APIServer from "./job_search_api/index.js"
+import chalk from "chalk"
 
 mongoose.connect('mongodb://localhost:27017/JobApplicationTracker');
 
 const app = express();
 app.use(cors())
 app.use(express.json())
-app.listen(4000)
+app.listen(4000, () => {
+    console.log(chalk.green('Server listening on port 4000'));
+} )
+
+APIServer.listen(3000, () => {
+    console.log(chalk.green('Server listening on port 3000'));
+} )
---
+++ job_search_api/config.js
@@ -0,0 +1,7 @@
+const config = {
+    APP_ID: process.env.APP_ID,
+    API_KEY: process.env.API_KEY,
+    BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+    BASE_PARAMS: 'search/1?results_per_page=50&content-type=application/json',
+}
+export default config;
