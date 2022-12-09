4d5eea7f8e72737154fe38f536934a13387e7516
Riiyabatra <batra.riy@northeastern.edu>
2022-12-09 17:49:35 -0500

    adding extra job fields

--- job/jobs-schema.js
+++ job/jobs-schema.js
@@ -2,16 +2,22 @@ import mongoose from "mongoose";
 
 const jobsSchema = mongoose.Schema({
     positionName: {type: String},
+    description: String,
+    term: String,
     positionType: {type: String, enum: ['Coop', 'Internship', 'Full Time']},
     numOpenings: Number,
+    location: String,
+    paymentType: String,
     dateOfPosting: Date,
     deadlineToApply: Date,
     company: {
          type: String,
          ref: 'CompanyModel' },
     skillsReqd: Array,
+    startDate: Date,
     jobLength: Number,
     pay: Number,
+    applicationDeadline: Date
 }, {collection: 'Jobs'})
 
 export default jobsSchema
