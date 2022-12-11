5f4a7b83d9d5115c5cd959d8e0044907a3b18906
Riiyabatra <batra.riy@northeastern.edu>
2022-12-11 18:34:40 -0500

    Joined 2 collections

--- application/application-dao.js
+++ application/application-dao.js
@@ -1,21 +1,21 @@
 import applicationModel from "./application-model.js";
 
 export const findByJobId = (jobId) => {
-    return applicationModel.find({jobId})
+    return applicationModel.find({jobId}).populate('job').populate('user')
 }
 
 export const findApplicationByUserId = (uid) => {
-    return applicationModel.find({user : uid})
+    return applicationModel.find({user : uid}).populate('job').populate('user')
 }
 
 export const findApplicationByJobId = (jid) => {
-    return applicationModel.find({job : jid}).populate('user')
+    return applicationModel.find({job : jid}).populate('job').populate('user')
 }
 export const findByEmail = (email) =>
-    applicationModel.find({email})
+    applicationModel.find({email}).populate('job').populate('user')
 
 export const findAllApplications = () => {
-    return applicationModel.find().populate('job')
+    return applicationModel.find().populate('job').populate('user')
 }
 
 export const createApplication = (application) =>
