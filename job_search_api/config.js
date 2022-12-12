66c729315b2591f76fdccd4e8b207a50d44186a6
Gowtham Potnuru <gouthamcall@gmail.com>
2022-12-11 23:19:32 -0500

    updated api keys

--- job_search_api/config.js
+++ job_search_api/config.js
@@ -1,6 +1,6 @@
 const config = {
-    APP_ID: process.env.APP_ID,
-    API_KEY: process.env.API_KEY,
+    APP_ID: "2dcdc3a1",
+    API_KEY: "4dc58dd72dcc742ab334d05c37cd8569",
     BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
     BASE_PARAMS: 'search/1?results_per_page=50&content-type=application/json',
 }
